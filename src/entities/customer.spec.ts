b3d6423833e46140452b97663665a47a6ec37a73
Rafael Barreto <rafaelhbarreto@gmail.com>
2022-10-27 06:25:05 -0300

    add customer reward points

--- src/entities/customer.spec.ts
+++ src/entities/customer.spec.ts
@@ -43,5 +43,18 @@ describe("Customer unit tests", () => {
     customer.deactivate(); 
 
     expect(customer.isActive()).toBe(false); 
-  })
+  }); 
+
+  it('should add reward points', () => {
+    const customer = new Customer('123', 'John doe'); 
+    expect(customer.rewardPoints).toBe(0); 
+    
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10); 
+    
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20); 
+  });
+  
+
 }); 
